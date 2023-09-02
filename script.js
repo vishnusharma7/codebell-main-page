dcda9fe8dc4935fc94f3c7f622a8bbaa3b504037
Vishnu Sharma <vs510514@gmail.com>
2023-09-02 21:54:38 +0530

    Nav bar UI changed

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ document.addEventListener('DOMContentLoaded', function () {
         menuPopup.style.display = 'none';
     }
 });
+
+
+
+  const nav = document.querySelector('.nav');
+
+  window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+
+    // Calculate the opacity and blur values based on the scroll position
+    const opacity = Math.min(0.8, scrollY / 200); // Adjust 200 to control when the effect starts
+    const blur = Math.min(5, scrollY / 10); // Adjust 10 to control the blur intensity
+
+    // Apply the updated styles to the navbar
+    nav.style.backgroundColor = `rgba(255, 255, 255, ${0})`;
+    nav.style.backdropFilter = `blur(${20}px)`;
+  });
+
